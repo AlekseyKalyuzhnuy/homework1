6437bf63cc569d2a686c419225ea83e95528f7b9
Алексей Калюжный <zero2502@gmail.com>
2022-08-09 15:52:36 +0300

    task6 cmplete

---
+++ task4/Program.cs
@@ -0,0 +1,32 @@
+﻿int numberA =22;
+int numberB =3;
+int numberC =9;
+int min =0;
+int max =0;
+
+if (numberA > numberB)
+{
+   max= numberA; 
+   min= numberB;
+}
+else
+{
+    max=numberB;
+    min=numberA;
+}
+
+if (max < numberC)
+{
+    max=numberC;
+}
+else
+{ 
+  if (numberC< min)
+  {
+    min=numberC;
+  }
+}
+  Console.Write("min= ");
+  Console.WriteLine(min);
+  Console.Write("max= ");
+  Console.WriteLine(max);
---
+++ task6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число");
+int num = int.Parse(Console.ReadLine());
+if (num % 2==0)
+{
+    Console.Write(num);
+    Console.WriteLine("  является четным числом");
+}
+else
+{
+    Console.Write(num);
+    Console.WriteLine("  является нечетным числом");
+}
