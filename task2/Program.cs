09facf6de0f37fcbce177a38ab7aa2c65d3dc3eb
Алексей Калюжный <zero2502@gmail.com>
2022-08-09 10:48:19 +0300

    Task2

---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿int numberA=-9;
+int numberB=-3;
+if(numberA> numberB)
+{ 
+   int max= numberA;
+   Console.Write("max= ");
+   Console.WriteLine(max);
+}
+if(numberA< numberB)
+{
+   int max = numberB;
+   Console.Write("max= ");
+   Console.WriteLine(max);
+}
+if(numberA == numberB)
+{
+    Console.WriteLine("no maximum");
+}
