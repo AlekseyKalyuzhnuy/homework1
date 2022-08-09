50ccea9e5939a30055b698cb058e147263cb6bdf
Алексей Калюжный <zero2502@gmail.com>
2022-08-09 13:40:56 +0300

    add files

--- task2/Program.cs
+++ task2/Program.cs
@@ -3,14 +3,21 @@ int numberB=-3;
 if(numberA> numberB)
 { 
    int max= numberA;
+   int min= numberB;
    Console.Write("max= ");
    Console.WriteLine(max);
+   Console.Write("min= ");
+   Console.WriteLine(min);
 }
 if(numberA< numberB)
 {
    int max = numberB;
+   int min = numberA;
    Console.Write("max= ");
    Console.WriteLine(max);
+   Console.Write("min= ");
+   Console.WriteLine(min);
+
 }
 if(numberA == numberB)
 {
