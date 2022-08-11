156da3c812a40cce3a03b4f66283b85e0a25d2e0
Алексей Калюжный <zero2502@gmail.com>
2022-08-11 18:28:40 +0300

    return main branch

---
+++ task15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите день недели ");
+int num = int.Parse(Console.ReadLine());
+if (num == 6 | num== 7) 
+{
+    Console.WriteLine("Выходной день");
+}
+else
+{
+    Console.WriteLine("Рабочий день");
+}
+
+
