faf59c9fc2a92bd68507369f9a97d58d3eaefe45
Алексей Калюжный <zero2502@gmail.com>
2022-08-11 15:38:49 +0300

    tsak13 complete

---
+++ task13/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите число до 100 000  ");
+int num = int.Parse(Console.ReadLine());
+int num1=0;
+if ((num>99) && (num<1000))
+{      
+num1=num % 10;
+}
+if((num>999) && (num< 10000))
+{
+  num1=num %100/10;
+}    
+if((num>9999) && (num<100000))
+{
+num1=num/100%10;
+}
+if(num<100)
+{
+Console.WriteLine("третьей цифры нет");
+}
+if(num1>0)
+{
+  Console.WriteLine(num1);  
+}
