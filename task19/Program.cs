137c1a29e55d97a99c834f2cc7ed71b6b5670e7c
Алексей Калюжный <zero2502@gmail.com>
2022-08-15 19:12:00 +0300

    test

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,20 +1,30 @@
 ﻿Console.Write("Введите пятизначное число ");
 int num = int.Parse(Console.ReadLine());
+int num1=0; 
+int num2=0;
+int num3=0;
+int num4=0;
+int num5=0; 
 if (num < 10000 | num >99999)
 {
     Console.WriteLine("Ввведенное число не является пятизначным");
-    break;
 }
  else
 {
-    int num1=num/10000;
-    int num2=num/1000%10;
-    int num3=num/100%10;
-    int num4=num%100;
-    int num5=num%10;
-    Console.WriteLine(num1);
+    num1=num/10000;
+    num2=num/1000%10;
+    num3=num/100%10;
+    num4=num%100/10;
+    num5=num%10;
+    Console.WriteLine(num1)
     Console.WriteLine(num2);
     Console.WriteLine(num3);
     Console.WriteLine(num4);
     Console.WriteLine(num5);
 }
+if((num1==num5) &&  (num2==num4))
+{
+    Console.WriteLine ({num} "-является палинромом");
+}
+
+
