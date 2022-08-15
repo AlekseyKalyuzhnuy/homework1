bf6c20121107d0eaed8420bdcb4b2d771ca522af
Алексей Калюжный <zero2502@gmail.com>
2022-08-15 19:43:49 +0300

    task23 complete

---
+++ task23/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+int k=num;
+if (num<0) k=-num;
+for(int i=1; i<=k; i++)
+Console.WriteLine($"i={Math.Pow(i,3)}");
