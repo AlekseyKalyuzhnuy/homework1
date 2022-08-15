79283c84c2b5a0a2854534d708b377073b2cbab1
Алексей Калюжный <zero2502@gmail.com>
2022-08-15 18:53:48 +0300

    task19

---
+++ task19/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите пятизначное число ");
+int num = int.Parse(Console.ReadLine());
+if (num < 10000 | num >99999)
+{
+    Console.WriteLine("Ввведенное число не является пятизначным");
+    break;
+}
+ else
+{
+    int num1=num/10000;
+    int num2=num/1000%10;
+    int num3=num/100%10;
+    int num4=num%100;
+    int num5=num%10;
+    Console.WriteLine(num1);
+    Console.WriteLine(num2);
+    Console.WriteLine(num3);
+    Console.WriteLine(num4);
+    Console.WriteLine(num5);
+}
