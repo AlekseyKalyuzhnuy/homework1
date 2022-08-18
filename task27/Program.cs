67e9cfc7aff48b58bbfa5b54b0f89d22949b10f8
Алексей Калюжный <zero2502@gmail.com>
2022-08-18 14:48:57 +0300

    task27 complete

---
+++ task27/Program.cs
@@ -0,0 +1,21 @@
+﻿int GetSum(int number)
+
+{
+    int num2=number;
+    int num1=number;
+    int sum=0;
+    while(num1 >0)
+    {
+        num2 = num1%10;
+        sum +=num2;
+        num1 /=10;
+    }
+    return sum;
+
+}
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine(GetSum(num));
+// считает сумму цифр в числе
