8d229f2e6384dbac63348be3536adfc64be75aab
Алексей Калюжный <zero2502@gmail.com>
2022-08-18 15:25:25 +0300

    task25completevCasMultypy

--- task25!/Program.cs
+++ task25!/Program.cs
@@ -1,11 +1,12 @@
-﻿int GetMath(int num1, int num2);
+﻿int GetMath(int num1, int num2)
+
 {
-   int result = num1;
+   int result = 1;
    for (int i=1; i<=num2; i++)
    {
      result *= num1; 
-     return result;
    } 
+   return result;
 }
 
 Console.Clear();
@@ -15,3 +16,4 @@ Console.WriteLine("Введите число b ");
 int b = int.Parse(Console.ReadLine());
 
 Console.WriteLine(GetMath(a,b));
+// вводим два числа и возводим первое число в степень второго числа
