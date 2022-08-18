e75dc11b1a1141811d7c27c3276201a479cc1597
Алексей Калюжный <zero2502@gmail.com>
2022-08-18 14:14:47 +0300

    problems

---
+++ task25!/Program.cs
@@ -0,0 +1,17 @@
+﻿int GetMath(int num1, int num2);
+{
+   int result = num1;
+   for (int i=1; i<=num2; i++)
+   {
+     result *= num1; 
+     return result;
+   } 
+}
+
+Console.Clear();
+Console.WriteLine("Введите число a ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine(GetMath(a,b));
---
+++ task25/Program.cs
@@ -0,0 +1,15 @@
+﻿int GetMath(int a, int b);
+int a;
+int b;
+int result =1;
+int result =Math.Pow(a,b);
+return result;
+
+Console.Clear();
+Console.WriteLine("Введите число a ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine(GetMuth(a,b));
+
