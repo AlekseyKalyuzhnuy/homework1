3420c2166b30078349489326c2f64a18ba91f2e7
Алексей Калюжный <zero2502@gmail.com>
2022-08-18 22:40:32 +0300

    tak29 complete

---
+++ task29/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] GetBinaryArray(int size)
+{
+  int [] arr = new int [size];
+  for (int i=0; i<size; i++)
+  {
+  arr[i] = new Random().Next(100);
+  }
+
+  return arr;
+}
+
+int[] array = GetBinaryArray(8);
+
+Console.WriteLine("Массив из 8 элементов :");
+Console.WriteLine($"[{string.Join(",",array) }]");
+
+// for (int i =0; i< array.Length; i++)
+//{
+//  Console.Write(array[i]);
+//}
