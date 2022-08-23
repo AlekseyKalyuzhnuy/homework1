580ac3ac891f4cfa873a04c8226ab4839feacdfb
Алексей Калюжный <zero2502@gmail.com>
2022-08-23 10:14:42 +0300

    born massiv

---
+++ task34/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача №34 задать массив из случайных положительныз трехзначныих чисел;
+// Посчитать количество четныех числе в массиве;
+
+Console.Clear();
+Console.WriteLine("Введите число элементов массива");
+int num = int.Parse(Console.ReadLine());
+
+int[] array=GetArray(num);
+Console.WriteLine(String.Join(", ", array));
+
+
+int[] GetArray(int count)
+{
+    int[] array=new int[count];
+    for(int i=0; i<count; i++)
+    {
+       array[i]=new Random().Next(100,1000);
+    }
+    return array;
+}
