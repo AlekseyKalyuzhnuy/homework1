ab1ed7eec4bdaca18905ff4f0822394ae7baad0b
Алексей Калюжный <zero2502@gmail.com>
2022-08-23 13:40:36 +0300

    task34 complete

--- task34/Program.cs
+++ task34/Program.cs
@@ -8,13 +8,27 @@ int num = int.Parse(Console.ReadLine());
 int[] array=GetArray(num);
 Console.WriteLine(String.Join(", ", array));
 
+int summ=NumberPositive(array);
+Console.WriteLine($"количество четных чисел в массиве {summ} ");
+
 
 int[] GetArray(int count)
 {
     int[] array=new int[count];
     for(int i=0; i<count; i++)
     {
-       array[i]=new Random().Next(100,1000);
+       array[i]=new Random().Next(100,1000); 
     }
     return array;
 }
+
+int NumberPositive(int [] arr)
+{
+    int sum=0;
+    for(int i=0; i<array.Length; i++)
+    {
+      if (array[i]%10==0) sum+=1;
+    } 
+    return sum;
+}
+
