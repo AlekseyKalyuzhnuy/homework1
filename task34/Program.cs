b246c69d29fb9b99aa6789af99b4283761c248b5
Алексей Калюжный <zero2502@gmail.com>
2022-08-24 17:46:06 +0300

    mistake delete

--- task34/Program.cs
+++ task34/Program.cs
@@ -27,7 +27,7 @@ int NumberPositive(int [] arr)
     int sum=0;
     for(int i=0; i<array.Length; i++)
     {
-      if (array[i]%10==0) sum+=1;
+      if (array[i]%2==0) sum+=1;
     } 
     return sum;
 }
