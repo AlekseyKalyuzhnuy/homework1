35d3ac6af21d04869179ca6cc4f9779bc60a4516
Алексей Калюжный <zero2502@gmail.com>
2022-08-25 19:36:18 +0300

    task41 complete

---
+++ task41/Program.cs
@@ -0,0 +1,21 @@
+﻿// задача 41 пользователь вводит с клавиатуры М чисел
+// программа должна посчитать сколько "0" ввел пользователь;
+
+
+Console.Clear();
+Console.WriteLine(" Выбереите количество цифр для ввода ");
+int m = int.Parse(Console.ReadLine());
+int count=0;
+int positiv=0;
+while (count< m)
+{
+Console. WriteLine(" Введите число"); 
+int num = int.Parse(Console.ReadLine());
+count+=1;
+if (num>0)
+  {
+    positiv+=1; 
+  }
+}
+ Console.WriteLine($"количество положительных чисел -{positiv}");
+
