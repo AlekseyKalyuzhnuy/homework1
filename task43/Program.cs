db7556997cd4b5c1a7937ad77eb36fba2ffa5ad1
Алексей Калюжный <zero2502@gmail.com>
2022-08-25 19:50:38 +0300

    task43 complete

---
+++ task43/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача найти точки пересечения двух прямых, заданных уровнениями
+
+Console.Clear();
+Console.WriteLine(" Введите b1 ");
+double b1 = int.Parse(Console.ReadLine());
+Console.WriteLine(" Введите k1 ");
+double k1 = int.Parse(Console.ReadLine());
+Console.WriteLine(" Введите b2 ");
+double b2 = int.Parse(Console.ReadLine());
+Console.WriteLine(" Введите k2 ");
+int k2 = int.Parse(Console.ReadLine());
+
+double x=(b2-b1)/(k1-k2);
+double y=k1*x+b1;
+Console.WriteLine($"Точка пересечения двух прямых имеет координаты: x={x}, y={y}.");
